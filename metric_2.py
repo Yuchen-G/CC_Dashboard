b00be1c52756bed0a959b2d254b13d35bec51e80
Narain Yucel <yucelnarain@gmail.com>
2022-03-03 10:09:20 -0500

    reformatted plot to matplot with sns style

--- metric_2.py
+++ metric_2.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 df2 = pd.read_excel('mi_2021_oor_data.xlsx', sheet_name='Sheet1')
 df2 = df2[['COUNTY/METRO','Income needed to afford 1 bdrm FMR', 'Income needed to afford 2 bdrm FMR']]
+df2 = df2.dropna()
 
 #Toggle data display
 if st.checkbox('Show raw data'):
@@ -27,9 +29,24 @@ if st.checkbox('Show filtered data'):
 #g.despine(left=True)
 #g.set_axis_labels("Counties", "Annual Income to afford Fair Market Rent")
 #g.legend.set_title("")
-if st.checkbox('Show our county chart', value=True):
-    import seaborn as sns
-    st.subheader('Annual Income Needed to Afford FMR')
-    sns.catplot(data=df2, kind='bar',
-    x='COUNTY/METRO', y='value', hue='variable', palette='dark', alpha=.6, height=6)
-    st.pyplot()
+sns.set_style('whitegrid')
+if st.checkbox('Show our county chart'):
+    #import seaborn as sns
+    #st.subheader('Annual Income Needed to Afford FMR')
+    #sns.catplot(data=df2, kind='bar',
+    #x='COUNTY/METRO', y='value', hue='variable', palette='dark', alpha=.6, height=6)
+    #st.pyplot()
+    x = np.arange(len(df2['COUNTY/METRO'].unique()))
+    labels = list(df2['COUNTY/METRO'].unique())
+    width = 0.35
+    one_bd = df2.loc[df2['variable']=='Income needed to afford 1 bdrm FMR', 'value']
+    two_bd = df2.loc[df2['variable']=='Income needed to afford 2 bdrm FMR', 'value']
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, one_bd, width, label='1 bdrm')
+    rects2 = ax.bar(x + width/2, two_bd, width, label='2 bdrm')
+    ax.set_ylabel('Annual Income Needed to Afford 1 Bdrm FMR')
+    ax.set_title('Income for FMR by County - 1 and 2 Bedrooms')
+    ax.legend()
+    fig.tight_layout()
+    plt.xticks(x, labels)
+    st.pyplot(fig)
