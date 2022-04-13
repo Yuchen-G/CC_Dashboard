1c83cc8bd86f8b2d12f12d68140e37a018387081
Narain Yucel <yucelnarain@gmail.com>
2022-04-13 13:38:28 -0400

    update filepath

--- scripts/metric_2.py
+++ scripts/metric_2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-df2 = pd.read_excel('mi_2021_oor_data.xlsx', sheet_name='Sheet1')
+df2 = pd.read_excel('assets/mi_2021_oor_data.xlsx', sheet_name='Sheet1')
 df2 = df2[['COUNTY/METRO','Income needed to afford 1 bdrm FMR', 'Income needed to afford 2 bdrm FMR']]
 df2 = df2.dropna()
 
